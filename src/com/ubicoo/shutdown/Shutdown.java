f159418af0b7904c240fda2ce9bfcaf8f0f5acb2
Stefan Simroth <stefan.simroth@gmail.com>
2011-10-08 19:45:47 +0200

    code cleanup and formatting.

--- src/com/ubicoo/shutdown/Shutdown.java
+++ src/com/ubicoo/shutdown/Shutdown.java
@@ -1,83 +1,87 @@
 package com.ubicoo.shutdown;
 
 import java.io.DataOutputStream;
+import java.io.IOException;
 
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 
 public class Shutdown extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        //setContentView(R.layout.main);
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(R.string.are_you_sure_you_want_to_exit)
-               .setCancelable(false)
-               .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
-                   public void onClick(DialogInterface dialog, int id) {
-                	   Shutdown.this.shutdown();
-                   }
-               })
-               .setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
-                   public void onClick(DialogInterface dialog, int id) {
-                	   Shutdown.this.finish();
-                   }
-               });
-        AlertDialog alert = builder.create();
-        alert.show();
-    }
-    
-    private void shutdown() {
-    	runRootCommand("reboot -p");
-    	//super.startActivity(new Intent("android.intent.action.ACTION_REQUEST_SHUTDOWN"));
-    }
-    
-    private void handleError(Exception ex) {
-    	//TODO give UI feedback via dialog (plus show Exception details)
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(ex.getClass().getSimpleName() + ": " + ex.getMessage())
-               .setCancelable(false)
-               .setNeutralButton(R.string.ok, new DialogInterface.OnClickListener() {
-                   public void onClick(DialogInterface dialog, int id) {
-                	   Shutdown.this.finish();
-                   }
-               });
-        AlertDialog alert = builder.create();
-        alert.show();
-    	
-    	//finish();
-    }
-    
-    public boolean runRootCommand(String command) {
-        Process process = null;
-        DataOutputStream os = null;
-        try {
-            process = Runtime.getRuntime().exec("su");
-            os = new DataOutputStream(process.getOutputStream());
-            os.writeBytes(command+"\n");
-            os.writeBytes("exit\n");
-            os.flush();
-            process.waitFor();
-        } catch (Exception e) {
-            //Log.d("*** DEBUG ***", "Unexpected error - Here is what I know: "+e.getMessage());
-            handleError(e);
-        	return false;
-        }
-        finally {
-            try {
-                if (os != null) {
-                    os.close();
-                }
-                process.destroy();
-            } catch (Exception e) {
-                // nothing
-            }
-        }
-        return true;
-    }
+	private static final String TAG = Shutdown.class.getSimpleName();
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setMessage(R.string.really_shutdown).setCancelable(false)
+				.setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
+						Shutdown.this.shutdown();
+					}
+				}).setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
+						Shutdown.this.finish();
+					}
+				});
+		AlertDialog alert = builder.create();
+		alert.show();
+	}
+
+	private void shutdown() {
+		runRootCommand("reboot -p");
+
+		//this only works for system applications with special permissions
+		//startActivity(new Intent("android.intent.action.ACTION_REQUEST_SHUTDOWN"));
+	}
+
+	private void handleError(Exception ex) {
+		String msg = ex.getClass().getSimpleName() + ": " + ex.getMessage();
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setMessage(msg).setCancelable(false).setNeutralButton(R.string.ok, new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int id) {
+				Shutdown.this.finish();
+			}
+		});
+		AlertDialog alert = builder.create();
+		alert.show();
+	}
+
+	public boolean runRootCommand(String command) {
+		Process process = null;
+		DataOutputStream os = null;
+		try {
+			process = Runtime.getRuntime().exec("su");
+			os = new DataOutputStream(process.getOutputStream());
+			os.writeBytes(command + "\n");
+			os.writeBytes("exit\n");
+			os.flush();
+			process.waitFor();
+		} catch (IOException e) {
+			handleError(e);
+			return false;
+		} catch (SecurityException e) {
+			handleError(e);
+			return false;
+		} catch (Exception e) {
+			Log.e(TAG, "Unexpected error", e);
+			handleError(e);
+			return false;
+		} finally {
+			try {
+				if (os != null) {
+					os.close();
+				}
+				process.destroy();
+			} catch (Exception e) {
+				// nothing
+			}
+		}
+		return true;
+	}
 
 }
